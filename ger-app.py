f4c1871d67debca4d2425abfaac791ee4244ead8
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2022-08-28 00:48:00 +0600

    Update ger-app.py

--- ger-app.py
+++ ger-app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import language_tool_python
-tl = language_tool_python.LanguageTool('en-US')
-txt = "good mooorning sirr and medam my namee anderen i am from amerecia !"
-m = tl.check(txt)
-len(m)
+
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tool = language_tool_python.LanguageToolPublicAPI('en-US')
+    return tool
+
+
+tool = get_model()
+
+with st.form(key='my_form'):
+    prompt = st.text_area(label='Enter sentence', value=" ")
+    submit_button = st.form_submit_button(label='Submit')
+    if submit_button:
+        #m = tool.correct(prompt)
+        #st.write(m)
+        
+        m = tool.check(prompt)
+        st.len(m)
