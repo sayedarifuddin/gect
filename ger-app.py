53fefd3e395f071c119605fe9036ea2ac46fde6a
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2022-08-28 00:21:35 +0600

    Create ger-app.py

---
+++ ger-app.py
@@ -0,0 +1,14 @@
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tool = language_tool_python.LanguageTool('en-US')
+    return tool
+
+
+tool = get_model()
+
+with st.form(key='my_form'):
+    prompt = st.text_area(label='Enter sentence', value=" ")
+    submit_button = st.form_submit_button(label='Submit')
+    if submit_button:
+        m = tool.correct(prompt)
+        st.write(m)
