fe4fb417f17583ed03a1f427df290593e276d541
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2022-08-28 00:45:21 +0600

    Update ger-app.py

--- ger-app.py
+++ ger-app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import language_tool_python
-
-        tl = language_tool_python.LanguageTool('en-US')
-    
-        txt = "good mooorning sirr and medam my namee anderen i am from amerecia !"
-        m = tl.check(txt)
-        len(m)
+tl = language_tool_python.LanguageTool('en-US')
+txt = "good mooorning sirr and medam my namee anderen i am from amerecia !"
+m = tl.check(txt)
+len(m)
