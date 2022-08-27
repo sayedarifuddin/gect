77fb996334d7f9d1b9b5acbd2749581e0a29a2ae
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2022-08-28 01:14:19 +0600

    Update ger-app.py

--- ger-app.py
+++ ger-app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import language_tool_python
 
-
 @st.cache(allow_output_mutation=True)
 def get_model():
-    tool = language_check.LanguageTool('en-US')
+    tool = language_tool_python.LanguageToolPublicAPI('en-US')
     return tool
-
 
 tool = get_model()
 
@@ -14,8 +12,5 @@ with st.form(key='my_form'):
     prompt = st.text_area(label='Enter sentence', value=" ")
     submit_button = st.form_submit_button(label='Submit')
     if submit_button:
-        #m = tool.correct(prompt)
-        #st.write(m)
-        
         m = tool.check(prompt)
         st.write(len(m))
