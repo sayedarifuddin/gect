94b80feed5977e20e8a98c1853728a58d95570b8
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2022-08-28 00:45:02 +0600

    Update ger-app.py

--- ger-app.py
+++ ger-app.py
@@ -1,4 +1,6 @@
-       import language_tool_python
+import streamlit as st
+import language_tool_python
+
         tl = language_tool_python.LanguageTool('en-US')
     
         txt = "good mooorning sirr and medam my namee anderen i am from amerecia !"
